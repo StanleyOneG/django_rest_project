10c76378d3b2075fb7cf35289798154b2dd1e7bb
Stas Goncharuk <goncharuk.sm@yandex.ru>
2022-03-27 08:17:03 +0300

    Made url dependencies to api app

---
+++ ecom/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.authtoken import views
+from .views import home_view
+
+
+urlpatterns = [
+    path('', home_view, name='api.home')
+]
--- ecom/api/views.py
+++ ecom/api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def home_view(request):
+    return JsonResponse({'info': 'Django React Course', 'name': 'stas'})
